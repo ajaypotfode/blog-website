2e66a83f9511ac62858ae4fa7607601fec9fd9f2
ajay potfode <ajaypotfode3277@gmail.com>
2025-07-09 19:29:44 +0530

    made some Changes to get Cookie

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -39,7 +39,6 @@ export const POST = async (req: Request) => {
 
 
         response.cookies.set('blogToken', token, {
-            nam
             httpOnly: true,
             secure: true,
             sameSite: 'strict',
