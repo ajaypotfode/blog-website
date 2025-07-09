aa7e273eb6bece6634fc88d23c08bd1ed4f98489
ajay potfode <ajaypotfode3277@gmail.com>
2025-07-09 19:34:03 +0530

    made some Changes to get Cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,20 @@ import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
-    const token = await request.cookies.get('blogToken')?.value
+    const token = await request.cookies.get('blogToken')
 
 
     const protectedRoute = ['/admin/addblog', '/admin/blog-list', '/admin/subscribers']
     // console.log("midlware is having token  :", token?.value?);
 
-    if (!token && protectedRoute.some((route) => pathname.startsWith(route))) {
+    if (!token?.value && protectedRoute.some((route) => pathname.startsWith(route))) {
         // console.log("midlware token not having :", pathname);
         const redirectUrl = new URL('/login', request.url)
         redirectUrl.searchParams.set('error', 'login_required')
         return NextResponse.redirect(redirectUrl)
     }
 
-    if (token && (pathname === '/register' || pathname === '/login')) {
+    if (token?.value && (pathname === '/register' || pathname === '/login')) {
         return NextResponse.redirect(new URL('/', request.url))
     }
 
