ec591954848039e89cdfb500119eb9ac6449a59a
ajay potfode <ajaypotfode3277@gmail.com>
2025-07-09 19:16:08 +0530

    made changes to get Token Immidetly

--- src/hooks/useAuthData.ts
+++ src/hooks/useAuthData.ts
@@ -47,7 +47,6 @@ const UseAuthData = () => {
         if (loginFormValidate(loginData, dispatch)) {
             const response = await dispatch(loginUser(loginData)).unwrap()
             if (response.success) {
-                // router.push('/')
                 window.location.href = '/'
                 toast.success('user Loged In SuccessFully!!!')
             }
